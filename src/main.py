724531e6ddf693f40fdfc46bac13da404e3beedb
KLag-SF <k.lagopus.sf19@gmail.com>
2022-03-09 20:59:42 +0900

    beta test version

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 import discord
+
 import discordSettings
+import conoha_dict
 
 TOKEN = discordSettings.TOKEN
+C_DICT = conoha_dict.C_DICT
+WORDS = C_DICT.keys()
+IMG_DIR = "../asset/img/"
 
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    msg = message.content
+    if msg in WORDS:
+        imgPath = IMG_DIR + C_DICT[msg] + ".png"
+        await message.channel.send(file=discord.File(imgPath))
+    elif msg in C_DICT.values():
+        imgPath = IMG_DIR + msg + ".png"
+        await message.channel.send(file=discord.File(imgPath))
+
+client.run(TOKEN)
