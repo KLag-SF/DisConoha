61bae8d7d2fd94cbb174c465401aea791cdccfdb
KLag-SF <k.lagopus.sf19@gmail.com>
2022-03-17 23:44:28 +0900

    Add function to send img_ID list

--- src/main.py
+++ src/main.py
@@ -28,6 +28,9 @@ async def on_message(message):
     elif msg == "!words":
         word_list = conoha_dict.get_word_list()
         await message.channel.send(word_list)
+    elif msg == "!id" or msg == "!ids":
+        id_list = conoha_dict.get_imgID_list()
+        await message.channel.send(id_list)
 
 def daemonize(client):
     pid = os.fork()
