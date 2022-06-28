0919b3e6412456364031d6bfaa0336c5b981ec29
KLag-SF <k.lagopus.sf19@gmail.com>
2022-06-28 12:30:44 +0900

    Imprement ReleaseNote notifier

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+from datetime import date
 import os
+import shutil
 import sys
+from time import strftime
 
 import discord
 
@@ -10,6 +13,7 @@ import conoha_dict
 TOKEN = discordSettings.TOKEN
 C_DICT = conoha_dict.C_DICT
 WORDS = C_DICT.keys()
+CHANNELS = []
 IMG_DIR = "/opt/disConoha/assets/img/"
 
 client = discord.Client()
@@ -17,6 +21,7 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     msg = message.content
+
     if msg in WORDS:
         imgPath = IMG_DIR + C_DICT[msg] + ".png"
         await message.channel.send(file=discord.File(imgPath))
@@ -31,20 +36,52 @@ async def on_message(message):
     elif msg == "!id" or msg == "!ids":
         id_list = conoha_dict.get_imgID_list()
         await message.channel.send(id_list)
+    
+    if message.channel.id not in CHANNELS:
+        with open("channels.dat", "w") as f:
+            f.write(f"{message.channel.id}\n")
 
 def daemonize(client):
     pid = os.fork()
+
     if pid > 0:
-        with open('/var/run/disConoha.pid', 'w') as f:
+        with open("/var/run/disConoha.pid", "w") as f:
             f.write(str(pid)+"\n")
-    
+
         sys.exit()
+
     if pid == 0:
         client.run(TOKEN)
 
-if __name__ == '__main__':
-    if '-t' not in sys.argv:
+async def notify_update():
+    with open("release.txt", "r") as f:
+        msg = f.read()
+    
+    for id in CHANNELS:
+        ch = client.get_channel(id)
+        ch.send(msg)
+
+    if not os.path.exists("../assets/releases"):
+        os.makedirs("../assets/releases")
+    
+    today = date.today.strftime("%y%m%d")
+    fname = f"release_{today}.txt"
+    shutil.move("../release.txt", f"../assets/releases/{fname}")
+    
+
+def main():
+    if os.path.exists("../channels.dat") and os.path.isfile("../channels.dat"):
+        with open("../channels.dat", "r") as f:
+            CHANNELS = f.readlines()
+
+    if os.path.exists("../release.txt") and os.path.isfile("../release.txt"):
+        notify_update()
+
+    if "-t" not in sys.argv:
         while True:
             daemonize(client)
     else:
         client.run(TOKEN)
+
+if __name__ == "__main__":
+   main()
